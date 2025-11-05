24c556e4352e587a79f83ed77daabfa98db338c1
Arun Kumar <arukmr1990@gmail.com>
2025-11-05 19:09:52 +0530

    Updated Booking model with all required properties

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -6,8 +6,24 @@ namespace CarCleanz.Models
     public class Booking
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Phone { get; set; }
+
+        [Required]
+        public string CustomerName { get; set; }
+
+        [Required]
+        public string CarType { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime BookingDate { get; set; }
+
+        [Required]
+        public string ServiceType { get; set; }
     }
 }
