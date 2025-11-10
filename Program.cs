244d21388ad96dd5c914c5f6863faf05db6b3380
Arun Kumar <arukmr1990@gmail.com>
2025-11-10 09:20:58 +0530

    Fixed Program.cs with SQLite connection and DB ensure

--- Program.cs
+++ Program.cs
@@ -1,35 +1,40 @@
-using CarCleanz.Data;
+using CarCleanz.Data; // ? make sure this matches your namespace for ApplicationDbContext
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// ? Use writable temp directory in Render
+// ? Configure SQLite connection
 var connectionString = "Data Source=/tmp/carcleanz.db";
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 
+// ? Add MVC support
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-}
-
-app.UseStaticFiles();
-app.UseRouting();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-// ? Create DB + tables automatically at startup
+// ? Ensure database is created when the app starts
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     db.Database.EnsureCreated();
 }
+
+// ? Configure the HTTP request pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
